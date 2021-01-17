1a8a5b8558882af526eed44535d21566570fb6d6
Maciej Topolewski <maciej@top-project.com.pl>
2021-01-17 01:30:29 +0100

    zad 19 z trojkatem

--- py/zadds19.py
+++ py/zadds19.py
@@ -4,25 +4,28 @@
 class Figura():
     """klasa bazowa; wartosc1 = podstawa trojkata lub bok prostokata;
     wartosc2 = wysokosc trojkata lub drugi bok prostokata;
-    args = faktultatywnie dodatkowe boki trójkąta"""
+    wartosc3 = trzeci bok trójkąta"""
 
-    def __init__(self, wartosc1, wartosc2, *args):
+    def __init__(self, wartosc1, wartosc2, wartosc3):
         self.wartosc1 = wartosc1
         self.wartosc2 = wartosc2
-        self.args = args
+        self.wartosc3 = wartosc3
         self.wynik_pole = 0
         self.wynik_obwod = 0
 
 
     def calculate_geometry(self):
         self.wynik_pole = self.wartosc1 * self.wartosc2
-
+        self.wynik_obwod = self.wartosc1 + self.wartosc2 + self.wartosc3
 
 class Prostokat(Figura):
     """dziedziczy wynik z Figury"""
 
     def calculate_rectangle(self):
         return self.wynik_pole
+
+    def calculate_circuit(self):
+        return (self.wartosc1*2 + self.wartosc2*2)
 
 
 class Trojkat(Figura):
@@ -31,7 +34,14 @@ class Trojkat(Figura):
     def calculate_triangle(self):
         return (self.wynik_pole/2)
 
+    def calculate_circuit(self):
+        return self.wynik_obwod
 
-obj = Figura(19,12)
-print(Prostokat().calculate_rectangle())
-print(obj2.calculate_rectangle())
+obj = Prostokat(19,12,2)
+obj.calculate_geometry()
+print(obj.calculate_rectangle())
+print(obj.calculate_circuit())
+obj2 = Trojkat(19,12,2)
+obj2.calculate_geometry()
+print(obj2.calculate_triangle())
+print(obj2.calculate_circuit())

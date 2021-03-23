f9910817add8407878a8a4b2008d723b70fedd05
Maciej Topolewski <maciej@top-project.com.pl>
2021-03-23 15:33:58 +0100

    sorted data pandas

--- pd/pd2.py
+++ pd/pd2.py
@@ -4,4 +4,8 @@ df = pd.read_csv('dataset_pandas_pd.csv', delimiter=',', names = ['imię',
     'nazwisko', 'rok_urodzenia', 'miesiąc_urodzenia', 'dzień_urodzenia',
     'miejsce_urodzenia', 'oddział', 'departament', 'rozpoczęcie_rok',
     'rozpoczęcie_miesiąc', 'rozpoczęcie_dzień', 'doświadczenie'])
-print(df.nazwisko.max())
+print(df['nazwisko'].value_counts().idxmax())
+
+#alternative methods:
+# print(df.sort_values(by='nazwisko'))
+# print(df.groupby('nazwisko').count())
